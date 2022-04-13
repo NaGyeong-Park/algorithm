3626c35231550bf8658b0037100fe77e9e5652be
NaGyeong Park <97649363+NaGyeong-Park@users.noreply.github.com>
2022-04-13 21:14:40 +0900

    [Gold IV] Title: 여행 가자, Time: 88 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -0,0 +1,39 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+
+
+def find(x):
+    if x == parent[x]:
+        return x
+    parent[x] = find(parent[x])
+    return parent[x]
+
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+
+    if x > y:
+        parent[x] = y
+    else:
+        parent[y] = x
+
+
+N = int(sys.stdin.readline())
+M = int(sys.stdin.readline())
+parent = [i for i in range(N + 1)]
+for i in range(1, N + 1):
+    temp = list(map(int, sys.stdin.readline().split()))
+    for j in range(len(temp)):
+        if temp[j] == 1:
+            union(i, j + 1)
+travel = list(map(int, sys.stdin.readline().split()))
+c = 0
+
+for i in range(M - 1):
+    if find(travel[i]) != find(travel[i + 1]):
+        c = 1
+        print('NO')
+        break
+if c == 0:
+    print('YES')
