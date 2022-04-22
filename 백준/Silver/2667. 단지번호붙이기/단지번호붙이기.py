2c6deebd8d5628d51faf7f9f0e48c2ddc5efd380
NaGyeong Park <97649363+NaGyeong-Park@users.noreply.github.com>
2022-04-22 23:40:43 +0900

    [Silver I] Title: 단지번호붙이기, Time: 140 ms, Memory: 115672 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+def BFS(nx,ny):
+    cnt = 0
+    q = deque()
+    q.append([nx,ny])
+    v[nx][ny] = True
+    while q:
+        x, y = q.popleft()
+        cnt += 1
+        for i in range(4):
+            ax = x + dx[i]
+            ay = y + dy[i]
+            if 0<=ax<N and 0<=ay<N and arr[ax][ay] and not v[ax][ay]:
+                v[ax][ay] = True
+                q.append([ax,ay])
+    return cnt
+
+N = int(input())
+arr = [list(map(int, input())) for _ in range(N)]
+v = [[False]*N for _ in range(N)]
+dx = [-1,1,0,0]
+dy = [0,0,1,-1]
+result = []
+for i in range(N):
+    for j in range(N):
+        if arr[i][j] and not v[i][j]:
+            result.append(BFS(i,j))
+result.sort()
+print(len(result))
+for i in result:
+    print(i)
