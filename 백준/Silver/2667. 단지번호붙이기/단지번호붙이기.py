624e3807a81c587d4b8fec80e21a036e6ae59220
NaGyeong Park <97649363+NaGyeong-Park@users.noreply.github.com>
2022-06-17 13:22:29 +0900

    [Silver I] Title: 단지번호붙이기, Time: 72 ms, Memory: 31084 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,33 +1,33 @@
 import sys
-from collections import deque
 
-def BFS(start):
-    dx = [1,-1,0,0]
+def DFS(start):
+    dx = [1, -1, 0, 0]
     dy = [0,0,1,-1]
-    cnt = 0
 
-    q = deque([start])
-    check[start[0]][start[1]] = True
-    while q:
-        x, y = q.popleft()
-        cnt +=1
-        for k in range(4):
-            nx = x + dx[k]
-            ny = y + dy[k]
-            if 0<=nx<N and 0<=ny<N and not check[nx][ny] and MAP[nx][ny]:
-                q.append([nx,ny])
-                check[nx][ny] = True
-    result.append(cnt)
+    x, y = start
+    check[x][y] = True
+    global cnt
+    cnt += 1
+    for k in range(4):
+        nx = x + dx[k]
+        ny = y + dy[k]
+        if 0<=nx<N and 0<=ny<N and not check[nx][ny] and MAP[nx][ny]:
+            DFS([nx,ny])
+
 
 N = int(sys.stdin.readline())
 MAP = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(N)]
 check = [[False]*N for _ in range(N)]
 result = []
 
+cnt = 0
 for i in range(N):
     for j in range(N):
         if MAP[i][j] and not check[i][j]:
-            BFS([i,j])
+            DFS([i,j])
+            result.append(cnt)
+            cnt = 0
+
 result.sort()
 print(len(result))
 print("\n".join(map(str,result)))
