1479b021fd206ebb7d3851ea95087e3c32a34b6c
NaGyeongPark <ngp.july@gmail.com>
2022-06-17 16:19:05 +0900

    DFS & BFS

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -13,7 +13,23 @@ def DFS(start):
         ny = y + dy[k]
         if 0<=nx<N and 0<=ny<N and not check[nx][ny] and MAP[nx][ny]:
             DFS([nx,ny])
-
+# def BFS(start):
+#     dx = [1,-1,0,0]
+#     dy = [0,0,1,-1]
+#     cnt = 0
+#
+#     q = deque([start])
+#     check[start[0]][start[1]] = True
+#     while q:
+#         x, y = q.popleft()
+#         cnt +=1
+#         for k in range(4):
+#             nx = x + dx[k]
+#             ny = y + dy[k]
+#             if 0<=nx<N and 0<=ny<N and not check[nx][ny] and MAP[nx][ny]:
+#                 q.append([nx,ny])
+#                 check[nx][ny] = True
+#     result.append(cnt)
 
 N = int(sys.stdin.readline())
 MAP = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(N)]
