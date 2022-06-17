c97ab791a598878dcd3c712335d178e9786166a2
NaGyeong Park <97649363+NaGyeong-Park@users.noreply.github.com>
2022-06-17 12:50:00 +0900

    [Silver I] Title: 단지번호붙이기, Time: 88 ms, Memory: 32468 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,38 +1,33 @@
 import sys
 from collections import deque
-sys.stdin = open('input.txt')
 
-T = int(input())
-
-
-def BFS(nx,ny):
+def BFS(start):
+    dx = [1,-1,0,0]
+    dy = [0,0,1,-1]
     cnt = 0
-    q = deque()
-    q.append([nx,ny])
-    v[nx][ny] = True
+
+    q = deque([start])
+    check[start[0]][start[1]] = True
     while q:
         x, y = q.popleft()
-        cnt += 1
-        for i in range(4):
-            ax = x + dx[i]
-            ay = y + dy[i]
-            if 0<=ax<N and 0<=ay<N and arr[ax][ay] and not v[ax][ay]:
-                v[ax][ay] = True
-                q.append([ax,ay])
-    return cnt
-for tc in range(1, T + 1):
+        cnt +=1
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if 0<=nx<N and 0<=ny<N and not check[nx][ny] and MAP[nx][ny]:
+                q.append([nx,ny])
+                check[nx][ny] = True
+    result.append(cnt)
 
-    N = int(input())
-    arr = [list(map(int, input())) for _ in range(N)]
-    v = [[False]*N for _ in range(N)]
-    dx = [-1,1,0,0]
-    dy = [0,0,1,-1]
-    result = []
-    for i in range(N):
-        for j in range(N):
-            if arr[i][j] and not v[i][j]:
-                result.append(BFS(i,j))
-    result.sort()
-    print(len(result))
-    for i in result:
-        print(i)
+N = int(sys.stdin.readline())
+MAP = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(N)]
+check = [[False]*N for _ in range(N)]
+result = []
+
+for i in range(N):
+    for j in range(N):
+        if MAP[i][j] and not check[i][j]:
+            BFS([i,j])
+result.sort()
+print(len(result))
+print("\n".join(map(str,result)))
