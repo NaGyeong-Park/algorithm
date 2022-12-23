cfbcee3b86f5486ddc29be0e6c3aad4a2bbe6055
NaGyeong Park <ngp.july@gmail.com>
2022-12-23 14:54:02 +0900

    algo: implementation-시각

---
+++ this_is_code_test/Implementation/시각.py
@@ -0,0 +1,14 @@
+H = int(input())
+cnt = 0
+for i in range(H+1):
+    if '3' in str(i):
+            cnt += 60*60
+    else: 
+        for j in range(60):
+            if '3' in str(j):
+                cnt += 60
+            else :
+                for k in range(60):
+                    if '3' in str(k):
+                        cnt+=1
+print(cnt)
